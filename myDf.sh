a4192604cd246f2fddcfceda5e0334cc4d5389b4
Eric <ericmarceau@rogers.com>
2020-11-07 15:38:20 -0500

    Create myDf.sh
    Report output of "df" command such that drive labels are sorted alphabetically and all mounted drives are at bottom.  Option '--all' reports non-disk filesystems and '--debug' reports raw input before displaying expected report.
    
    Sample output:
    
            Filesystem      Size  Used Avail Use% Mounted on
    
            /dev/sdb3       192G  9.8G  172G   6% /
            /dev/sdb7       289G  167G  108G  61% /DB001_F2
            /dev/sdb8       289G  243G   31G  89% /DB001_F3
            /dev/sdb9       289G  244G   31G  89% /DB001_F4
            /dev/sdb12      193G   27G  157G  15% /DB001_F5
            /dev/sdb13      193G  155G   28G  85% /DB001_F6
            /dev/sdb14      289G  185G   89G  68% /DB001_F7
            /dev/sdb4        92G   28G   60G  32% /DB001_F8
    
            /dev/sdd3       275G  7.1G  254G   3% /media/username/DB002_F1
            /dev/sdd4       275G  184G   77G  71% /media/username/DB002_F2
            /dev/sdd5       288G  237G   37G  87% /media/username/DB002_F3
            /dev/sdd6       288G  244G   30G  90% /media/username/DB002_F4
            /dev/sdd7       192G  158G   24G  87% /media/username/DB002_F5
            /dev/sdd8       192G  155G   28G  86% /media/username/DB002_F6
            /dev/sdd9       288G  220G   54G  81% /media/username/DB002_F7
            /dev/sdd10       31G   26G  3.6G  88% /media/username/DB002_F8
    
            /dev/sdc3       454G   14G  418G   4% /media/username/DB003_F1
    
            /dev/sde2       108G  9.8G   93G  10% /media/username/DB004_F1
    
            /dev/sda4       295G   63M  280G   1% /media/username/DB005_F2
            /dev/sda5       295G   63M  280G   1% /media/username/DB005_F3
            /dev/sda6       295G   63M  280G   1% /media/username/DB005_F4
            /dev/sda7       295G   63M  280G   1% /media/username/DB005_F5
            /dev/sda8       295G   63M  280G   1% /media/username/DB005_F6
            /dev/sda9       295G   63M  280G   1% /media/username/DB005_F7
            /dev/sda10      295G   63M  280G   1% /media/username/DB005_F8
    
            /dev/sda11      295G   63M  280G   1% /media/username/DB006_F1
            /dev/sda12      295G   63M  280G   1% /media/username/DB006_F2
            /dev/sda13      295G   63M  280G   1% /media/username/DB006_F3
            /dev/sda14      295G   63M  280G   1% /media/username/DB006_F4
            /dev/sda15      316G   67M  300G   1% /media/username/DB006_F5
    
            Filesystem      Size  Used Avail Use% Mounted on

---
+++ myDf.sh
@@ -0,0 +1,109 @@
+#!/bin/sh
+#########################################################################
+###
+###	$Id: myDf.sh,v 1.2 2020/10/22 22:52:13 root Exp $
+###
+###	Report output of "df" command such that drive labels are sorted alphabetically and all mounted drives are at bottom.  Option '--all' reports non-disk filesystems and '--debug' reports raw input before displaying expected report.
+###
+#########################################################################
+
+#FUTURES: redo list so that root "/" is reported just before others and with its own drive label.
+
+#DevStat=PROD
+#2018-03-18
+
+TMP=/tmp/`basename $0 ".sh"`.$$
+
+onlyHard=1
+debug=0
+mirrors=0
+
+if [ $# -gt 0 ]
+then
+	case $1 in
+		--mirrors )	mirrors=1	; shift ;;
+		--all )		onlyHard=0	; shift ;;
+		--debug )	debug=1		; shift ;;
+		* ) ;;
+	esac 
+fi
+
+rm -f ${TMP}.*
+df -h >${TMP}.raw
+
+header=`head -1 ${TMP}.raw `
+echo "\n\t${header}"									> ${TMP}.2
+
+if [ ${mirrors} -eq 1 ]
+then
+	tail -n +2	${TMP}.raw | sort -k1,1 | sort -k6,6				> ${TMP}.1
+else
+	tail -n +2	${TMP}.raw | sort -k6						> ${TMP}.1
+	if [ ${onlyHard} != 1 ]
+	then
+		grep -v '/sd' ${TMP}.1 | grep -v '/media' | sort -n -k1,1 | awk '{ printf("\t%s\n", $0 ) }'	>>${TMP}.4 
+
+		#/dev/sda1       288G   16G  257G   6% /media/ericthered/DB001_F1
+		cat ${TMP}.4 | awk 'BEGIN{ INDX=0 }{
+			REF=substr($1,1,4) ;
+			if( REF == INDX ){
+				printf("\t%-12s %7s %5s %5s %4s %s\n", $1, $2, $3, $4, $5, $6 ) ;
+			}else{
+				printf("\n\t%-12s %7s %5s %5s %4s %s\n", $1, $2, $3, $4, $5, $6 ) ;
+				INDX=REF ;
+			} ;
+		}'									>>${TMP}.2
+
+		echo "\n\t${header}"							>>${TMP}.2
+	fi
+fi
+
+if [ ${debug} -eq 1 ]
+then
+	cat ${TMP}.1
+	echo "=========================================\n"
+fi
+
+if [ ${mirrors} -eq 1 ]
+then
+	grep '/sd'	${TMP}.1 							>>${TMP}.3
+
+	cat ${TMP}.3 | awk '{ 
+		split($6,var,"/") ;
+		split(var[4],dar,"_") ;
+		REF=substr(dar[2],2) ; if( length(REF) == 0 ){ REF=0 } ;
+		printf("%s|%s|%s\n", REF, $6, $0 ) ;
+	}' | sort | awk -F \| '{ printf("%s\n", $3 ) }' |
+	awk 'BEGIN{ INDX=-1 }{
+		split($6,var,"/") ;
+		split(var[4],dar,"_") ;
+		REF=substr(dar[2],2) ; if( length(REF) == 0 ){ REF=0 } ;
+		if( REF == INDX ){
+			printf("\t%-12s %7s %5s %5s %4s %s\n", $1, $2, $3, $4, $5, $6 ) ;
+		}else{
+			printf("\n\t%-12s %7s %5s %5s %4s %s\n", $1, $2, $3, $4, $5, $6 ) ; 
+			INDX=REF ;
+		} ;
+	}'										>>${TMP}.2 
+	echo "\n\t${header}\n"								>>${TMP}.2
+else
+	grep '/sd'	${TMP}.1 | grep -v '/media'					>>${TMP}.3
+	grep '/sd'	${TMP}.1 | grep    '/media'					>>${TMP}.3
+
+	cat ${TMP}.3 | awk 'BEGIN{ INDX=0 }{
+		split($6,var,"/") ;
+		split(var[4],dar,"_") ;
+		REF=substr(dar[1],5) ;
+		if( REF == INDX ){
+			printf("\t%-12s %7s %5s %5s %4s %s\n", $1, $2, $3, $4, $5, $6 ) ;
+		}else{
+			printf("\n\t%-12s %7s %5s %5s %4s %s\n", $1, $2, $3, $4, $5, $6 ) ; 
+			INDX=REF ;
+		} ;
+	}'										>>${TMP}.2 
+	echo "\n\t${header}\n"								>>${TMP}.2
+fi
+
+cat ${TMP}.2
+
+rm -f ${TMP}.*
