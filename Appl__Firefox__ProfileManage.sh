cd33af159cbf354fe784bbedce159f90242d6a83
Eric <ericmarceau@rogers.com>
2024-09-11 14:25:24 -0400

    Add files via upload

---
+++ Appl__Firefox__ProfileManage.sh
@@ -0,0 +1,12 @@
+
+####################################################################################################
+###
+###	$Id: Appl__Firefox__ProfileManage.sh,v 1.1 2020/09/09 03:30:26 root Exp $
+###
+###	Script to start Firefox with the Profile Manager option that allows selection of the Profile to use from among those that are defined for the user.
+###
+####################################################################################################
+
+#DevStat=PROD
+
+firefox -ProfileManager
