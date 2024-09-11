a1bfc63bcf00557fe6885cd0b3edba90cb2d7c25
Eric <ericmarceau@rogers.com>
2024-09-11 17:45:14 -0400

    Add files via upload

---
+++ HW_Admin__ProbeHardwareAndReport.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+###	This will probe hardware, report on components, identify issues.
+###	Report is web page, uploaded to Application web site.
+###	URL reported will open the relevant report.
+
+hw-probe -all -upload
