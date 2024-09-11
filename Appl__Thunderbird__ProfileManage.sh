4f77147ffd9df4a3d805778de9ef0ee915b1680e
Eric <ericmarceau@rogers.com>
2024-09-11 14:28:25 -0400

    Add files via upload

---
+++ Appl__Thunderbird__ProfileManage.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+####################################################################################################
+###
+###	$Id: Appl__Thunderbird__ProfileManage.sh,v 1.2 2022/08/17 21:26:40 root Exp $
+###
+###	Script to start Thunderbird with the Profile Manager option that allows selection of the Profile to use from among those that are defined for the user.
+###
+####################################################################################################
+
+#DevStat=PROD
+
+thunderbird -ProfileManager
